08d3cf50a784593e42614a621b87f3167f698148
Kolja Hopfmann <k.hopfmann@hotmail.de>
2017-05-05 18:30:22 +0200

    intelliJ push test

--- src/main/java/runner/MainRunner.java
+++ src/main/java/runner/MainRunner.java
@@ -5,19 +5,23 @@ import sx.blah.discord.api.IDiscordClient;
 public class MainRunner
 {
 
-	static IDiscordClient cli;
+    private static IDiscordClient cli;
 
 	public static void main(String[] args)
 	{
 
 		cli = BotUtils.getBuiltDiscordClient("MzA2NDA3MjU1NDU5MjMzNzky.C-DTiQ.ByLgE38pRYAG7n6TET7i2d9Ef0k");
 
-		// Register a listener via the EventSubscriber annotation which allows
-		// for organisation and delegation of events
+		/*
+		Register a listener via the EventSubscriber annotation which allows
+		for organisation and delegation of events
+		*/
 		cli.getDispatcher().registerListener(new MyEvents());
 
-		// Only login after all events are registered otherwise some may be
-		// missed.
+		/*
+		Only login after all events are registered otherwise some may be
+		missed.
+		*/
 		cli.logout();
 
 		// this keeps the bot from logging in multiple times.
